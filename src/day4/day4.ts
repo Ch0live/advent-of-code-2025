66ab9171727b231af7158b820afd906ba57108d8
Ch0live <dozeyis@gmail.com>
2025-12-04 16:52:43 +0000

    D4P1 solved

--- src/day4/day4.ts
+++ src/day4/day4.ts
@@ -1,15 +1,15 @@
 import { readFileSync } from "node:fs";
 import { join } from "node:path";
 
-const input = readFileSync(join(__dirname, "example.txt"), "utf8").trim().split("\r\n");
-console.log(input);
+const input = readFileSync(join(__dirname, "input.txt"), "utf8").trim().split("\r\n");
 
 /*
 PART 1
 --- Initial thoughts ---
+Can break down each check into 
+- Is it possible for grid position to have a neighbor in the direction?
+- If so, is it another roll or not? +1 if so
 */
-
-let sol = 0;
 
 const createGrid = (input: string[]): string[] => {
     const grid = new Array();
@@ -17,24 +17,57 @@ const createGrid = (input: string[]): string[] => {
     return grid;
 };
 
+const coordIsOnARoll = (grid: string[], coords: number[]) => {
+    const [i, j] = coords;
+    return grid[i][j] === "@";
+}
+
 const surroundingRollsGreaterThan = (limit: number, grid: string[], coords: number[]) => {
     const [i, j] = coords;
-    let n, ne, e, se, s, sw, w, nw = false;
-    if (i > 0 && j > 0) {
-        nw = i > 0 && j > 0 ? grid[i-1][j-1] === "@" : false;
-        n = i > 0 ? grid[i][j-1] === "@" : false;
-        ne = i < grid[i].length && j > 0 ? grid[i+1][j-1] === "@" : false;
-    }
+    let n, ne, e, se, s, sw, w, nw = 0;
+    n = i > 0
+        ? grid[i - 1][j] === "@"
+        ? 1 : 0 : 0;
+    ne =  i > 0 && j < grid[i].length
+        ? grid[i - 1][j + 1] === "@"
+        ? 1 : 0 : 0;
+    e = j < grid[i].length
+        ? grid[i][j + 1] === "@"
+        ? 1 : 0 : 0;
+    se = i < grid.length - 1 && j < grid[i].length - 1
+        ? grid[i + 1][j + 1] === "@"
+        ? 1 : 0 : 0;
+    // if (i < grid.length - 1 && j < grid[i].length - 1) { console.log(`SE checked, was ${grid[i+1][j+1]} in ij ${i+1}${j+1}`); }
+    s = i < grid.length - 1
+        ? grid[i + 1][j] === "@"
+        ? 1 : 0 : 0;
+    sw = i < grid.length - 1 && j > 0
+        ? grid[i + 1][j - 1] === "@"
+        ? 1 : 0 : 0;
+    w = j > 0
+        ? grid[i][j - 1] === "@"
+        ? 1 : 0 : 0;
+    nw = i > 0 && j > 0
+        ? grid[i - 1][j - 1] === "@"
+        ? 1 : 0 : 0;
+        console.log(`
+            ${nw} - ${n} - ${ne}\t\tRow index [${i}, ${j}]
+            ${w} - ${grid[i][j]} - ${e}\t\tRow[i] is ${grid[i]}, row[i][j] is ${grid[i][j]}
+            ${sw} - ${s} - ${se}\t\tRoll movable? ${(n + ne + e + se + s + sw + w + nw) < 4}\n`);
+    return (n + ne + e + se + s + sw + w + nw) < 4;
 }
 
 const grid = createGrid(input);
 let sol = 0;
-console.log(`Grid: ${grid}, row 1 col 1 is ${grid[1][1]}`);
+console.log("Grid:");
+console.log(input);
 
 for (let i = 0; i < input.length; i++) {
     for (let j = 0; j < input.length; j++) {
-        if (surroundingRollsGreaterThan(4, grid, [i, j])) {
+        if (coordIsOnARoll(grid, [i, j]) && surroundingRollsGreaterThan(4, grid, [i, j])) {
             sol++;
         }
     }
 }
+
+console.log(`Sol: ${sol}`);
