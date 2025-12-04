91acfe6b80e3411cf192f5b2fdbceafb5e60c126
Ch0live <dozeyis@gmail.com>
2025-12-04 08:34:33 +0000

    D4P1 Handle NW, N and NE steps

--- src/day4/day4.ts
+++ src/day4/day4.ts
@@ -17,5 +17,24 @@ const createGrid = (input: string[]): string[] => {
     return grid;
 };
 
+const surroundingRollsGreaterThan = (limit: number, grid: string[], coords: number[]) => {
+    const [i, j] = coords;
+    let n, ne, e, se, s, sw, w, nw = false;
+    if (i > 0 && j > 0) {
+        nw = i > 0 && j > 0 ? grid[i-1][j-1] === "@" : false;
+        n = i > 0 ? grid[i][j-1] === "@" : false;
+        ne = i < grid[i].length && j > 0 ? grid[i+1][j-1] === "@" : false;
+    }
+}
+
 const grid = createGrid(input);
+let sol = 0;
 console.log(`Grid: ${grid}, row 1 col 1 is ${grid[1][1]}`);
+
+for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input.length; j++) {
+        if (surroundingRollsGreaterThan(4, grid, [i, j])) {
+            sol++;
+        }
+    }
+}
